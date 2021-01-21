1ac4209f9bcbf96c83c780a737081c31d4d6e451
Dawid <76065698+Bleach-IT@users.noreply.github.com>
2021-01-21 10:29:29 +0100

    Add files via upload

---
+++ instrukcja_warunkowa_V1.py
@@ -0,0 +1,11 @@
+a=float(input("a="))
+b=float(input("b="))
+
+if a>b:
+    print("a jest większa od b")
+
+if a<b:
+    print("a jest mniejsza od b")   
+
+if a==b:
+    print("a jest równa b")    
---
+++ instrukcje_warunkwe_V2.py
@@ -0,0 +1,12 @@
+a=float(input("a="))
+b=float(input("b="))
+
+
+if a>b:
+    print("a jest większa od b")
+
+elif a<b:
+    print("a jest mniejsza od b")   
+
+elif a==b:
+    print("a jest równa b")    
